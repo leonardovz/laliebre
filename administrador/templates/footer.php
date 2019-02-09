63a28d56f6e8f3ae9516289cb4807e397d1a6eee
leonardo Vázquez <leonardovazquez81@gmail.com>
2019-02-09 13:59:27 -0600

    Se agrego la parte de administrador

--- administrador/templates/footer.php
+++ administrador/templates/footer.php
@@ -1,6 +1,6 @@
 
 <!-- jQuery 3 -->
-<script src="<?php echo $ruta;?>administrador/js/jquery.min.js"></script>
+
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo $ruta;?>administrador/js/bootstrap.min.js"></script>
 <script src="<?php echo $ruta;?>administrador/js/sweetalert2.all.min.js"></script>
@@ -12,6 +12,9 @@
 <script src="<?php echo $ruta;?>administrador/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo $ruta;?>administrador/js/demo.js"></script>
+
+<script src="<?php echo $ruta;?>administrador/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo $ruta;?>administrador/js/dataTables.bootstrap.min.js"></script>
 <script>
   $(document).ready(function () {
     $('.sidebar-menu').tree()
--- administrador/templates/header.php
+++ administrador/templates/header.php
@@ -8,6 +8,7 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?php echo $ruta;?>administrador/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $ruta;?>administrador/css/dataTables.bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo $ruta;?>administrador/css/font-awesome.min.css">
   <!-- Ionicons -->
@@ -20,4 +21,5 @@
   <link rel="stylesheet" href="<?php echo $ruta;?>administrador/css/_all-skins.min.css">
   <link rel="stylesheet" href="<?php echo $ruta;?>administrador/css/blue.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <script src="<?php echo $ruta;?>administrador/js/jquery.min.js"></script>
 </head>
